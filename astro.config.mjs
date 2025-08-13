eee112d691ddbc824b3d30c64def641c296f409b
Jasper van Boggelen <Jasper.van.Boggelen@aae-eu.tech>
2025-08-13 12:32:54 +0200

    Fix analytics page access and update logos to use favicon.svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://everdantia.art',
-  output: 'static',
+  output: 'server',
   adapter: netlify(),
   integrations: [sitemap()],
 });
