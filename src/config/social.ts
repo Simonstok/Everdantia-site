c5333d24fde5aabc6890a63baeeb3c103c59019b
Jasper van Boggelen <Jasper.van.Boggelen@aae-eu.tech>
2025-08-18 12:52:50 +0200

    push

---
+++ src/config/social.ts
@@ -0,0 +1,16 @@
+// Centralized social media links configuration
+// Update Discord invite link here when it expires (weekly)
+
+export const socialLinks = {
+  discord: 'https://discord.gg/enqAsyDU',
+  twitter: 'https://twitter.com/Everdantia',
+  instagram: 'https://instagram.com/EverdantiaNFT',
+  email: 'everdantia@gmail.com'
+};
+
+// Structured data social links array (for SEO)
+export const socialLinksArray = [
+  socialLinks.twitter,
+  socialLinks.discord,
+  socialLinks.instagram
+];
